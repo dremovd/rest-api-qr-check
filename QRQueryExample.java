26eb50b8dfa7e2ee1fcb99673975218ca99ee26c
Dmitry Dremov <dremovd@gmail.com>
2017-12-23 14:16:21 +0200

    Add example for java (Android).

---
+++ QRQueryExample.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+public class QRQueryExample {
+public static void main(String[] args) { 
+	ArrayList<String> barCodes = new ArrayList<String>();
+	barCodes.add("t=20171007T1024&s=129.90&fn=8710000100240299&i=40143&fp=1324042032&n=1"); 
+    for (int codeIndex = 0; codeIndex < barCodes.size(); ++codeIndex) {
+        String parameters = barCodes.get(codeIndex);
+        int newChecks = 0;
+        try {
+			String apiUrl = "https://rest-fns-check.herokuapp.com/check/" + parameters;
+            URL url = new URL(
+                apiUrl
+            );
+			System.out.println("API URL: " + apiUrl);
+
+            URLConnection urlc = url.openConnection();
+            urlc.setAllowUserInteraction(false);
+            urlc.setUseCaches(false);
+            urlc.setConnectTimeout(30000);
+            urlc.setReadTimeout(30000);
+            BufferedReader br = new BufferedReader(
+                new InputStreamReader(urlc.getInputStream())
+            );
+            String jsonResult = "";
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                jsonResult += line;
+            }
+            br.close();
+			
+            try {
+                JSONObject result = new JSONObject(jsonResult);
+                JSONArray items = (JSONArray) result.get("items");
+                String time = (String) result.get("check_time");
+                String check_description = (String) result.get("check_description");
+		        
+				System.out.println("Check: " + check_description);				
+                for (int i = 0; i < items.length(); ++i) {
+                    JSONArray itemRaw = (JSONArray) items.get(i);
+                    String name = (String) itemRaw.get(0);
+                    Float price = ((Number) itemRaw.get(1)).floatValue();
+                    Float count = ((Number) itemRaw.get(2)).floatValue();
+                    String category = (String) itemRaw.get(3);
+					System.out.println("Item: " + name + "\t" + category + "\t" + price.toString() + "\t" + count.toString());				
+                }
+				String rawResponse = (String) result.get("raw_response");
+				System.out.println("Raw response: " + rawResponse);								
+            } catch (org.json.JSONException e) {
+            }
+        } catch (MalformedURLException e) {
+        } catch (IOException e) {
+        }
+    }
+	
+    }
+}
+
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rest-api-qr-check
 Примеры использования API для получения электронных чеков по содержимому QR кода с классификацией
+## Java:
+javac -cp org.json.jar QRQueryExample.java
+java -cp '.:org.json.jar' QRQueryExample 
